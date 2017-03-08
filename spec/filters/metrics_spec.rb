ef423056b238b31ab28d18378647c463a36025e1
Gérald QUINTANA <gerald-externe.quintana@erdf.fr>
2017-03-08 06:27:09 +0100

    Fix host field
    Avoid doing a host resolution on each event
    Store host in host field instead of message

--- spec/filters/metrics_spec.rb
+++ spec/filters/metrics_spec.rb
@@ -42,6 +42,11 @@ describe LogStash::Filters::Metrics do
             end
           end
         end
+
+        it "should have host" do
+          insist { subject.length } == 1
+          reject { subject.first.get("host") }.nil?
+        end
       end
 
       context "on the second flush" do
